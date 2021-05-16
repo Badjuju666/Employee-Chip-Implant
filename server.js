c55b0a0c8aba51a3ab24ceb7bf1ba83ddc751814
Julian Graves <Juliangraves96@gmail.com>
2021-05-15 21:41:14 -0700

    everything running nicely made easier to understand

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const table = require('console.table');
+
 require('dotenv').config();
 
 let con = mysql.createConnection({
@@ -11,6 +12,34 @@ let con = mysql.createConnection({
     database: 'emp_datab'
 });
 
+let speak2ManagerPlz;
+let showDepartments;
+let showEmployees;
+let showRoles;
+
+con.connect(function (err) {
+    if(err){
+        console.error("error on connection" + err.stack); 
+        return;
+    }
+    con.query("SELECT * from employeetypes", function (error, res) {
+        speak2ManagerPlz = res.map(manager => ({ id: manager.manager_id, name: `${manager.first_name} ${manager.last_name}`, value: manager.id }))
+    })
+    
+    con.query("SELECT * from departmenttypes", function (error, res) {
+        showDepartments = res.map(depar => ({ name: `${depar.department_name}`, value: depar.id }))
+    })
+    
+    con.query("SELECT * from employeetypes", function (error, res) {
+        showEmployees = res.map(employ => ({ name: `${employ.first_name} ${employ.last_name}`, value: employ.manager_id}))
+    })
+
+    con.query("SELECT * from roletypes", function (error, res) {
+        showRoles = res.map(roler => ({value: roler.id, name : `${roler.title}`}))
+    })
+
+})
+
 //start by asking what they want robot
 function yourChoices() {
     inquirer.prompt([
@@ -18,27 +47,31 @@ function yourChoices() {
             name: 'choice',
             type: 'list',
             message: 'Main Menu: what do you need done?',
-            choices: ["View your departments", "View your roles", "View your employees", 
-            "Add your departments", "Add your roles", "Add your employee", "Update Employee Info", "All done!"]
+            choices: ["View your Departments.", "View the Roles.", "View your Employees.", 
+            "Add an Department.", "Add your Roles.", "Add a Employee.", "Update Info.", "All done!"]
         }
     ]).then(response => {
-        if (response.choice === "View your Departments"){
+        if (response.choice === "View your Departments."){
             tableD();
-        } else if(response.choice === "View the Roles"){
+        } else if(response.choice === "View the Roles."){
             tableR();
-        } else if(response.choice === "View your Employees" ){
+        } else if(response.choice === "View your Employees." ){
             tableE();
-        } else if(response.choice === "Add an Department"){
+        } else if(response.choice === "Add an Department."){
             addD();
-        } else if(response.choice === "Add a Role"){
+        } else if(response.choice === "Add your Roles."){
             addR();
-        } else if(response.choice === "Add a Employee"){
+        } else if(response.choice === "Add a Employee."){
             addE();
-        } else if(response.choice === "Update Employee Info"){
-            updateInfo();
+        } else if(response.choice === "Update Info."){
+            updateEmpInfo();
         } else if(response.choice === "All done!"){
             process.exit();
         }
+        // } else if(response.choice === "Update Info."){
+        //     updateRole();
+        // }
+        console.log({response})
     })
 }
 
@@ -49,7 +82,7 @@ function addD(){
             name: 'departmenttype',
             type: 'input',
             message: 'Name the Department that you are adding.'
-        },
+        }
     ]).then(response => {
         sqlQuery = `INSERT INTO departmenttypes (department_name) VALUES ('${response.departmenttype}');`
         con.query(sqlQuery, (err, res)=>{
@@ -74,14 +107,15 @@ function addR(){
             message: 'What is thier Salary for this Role?'
        },
        {
-            name: 'departmenttpye',
-            type: 'input',
-            message: 'Provide a ID for this Roles Department.'
+            name: 'deparid',
+            type: 'list',
+            message: 'What Department is this Role for?',
+            choices: showDepartments
        }        
     ]).then(response => {
-        con.query(`INSERT INTO roletypes (title, salary, department_id) VALUES ('${response.roletype}', '${response.salary}', ${response.departmenttype})`, (err, res) => {
+        con.query(`INSERT INTO roletypes (title, salary, department_id) VALUES ('${response.roletype}', '${response.salary}', ${response.deparid})`, (err, res) => {
             if(err) throw err;
-            console.log('New Department Made.')
+            console.log('New Role Made.')
             yourChoices();
         })
     })
@@ -107,38 +141,43 @@ function addE(){
         },
         {
             name: 'management',
-            type: 'input',
-            message: 'Enter the manager ID for this Employee.'
+            type: 'list',
+            message: 'Enter the manager ID for this Employee.',
+            choices: showEmployees
         }
-    ]),then(response => {
+    ]).then(response => {
         con.query(`INSERT INTO employeetypes (first_name, last_name, role_id, manager_id) VALUES ('${response.firstname}', '${response.lastname}', '${response.roletype}', '${response.management}')`, (err, res)=>{
             if(err) throw err;
-            console.log('added employee name')
+            console.log('New Employee made.')
             yourChoices();
         })
     })
 }
 
 //Update the Employees Info prompts
-function updateInfo(){
+function updateEmpInfo(){
     inquirer.prompt([
         {
-            name: 'updates',
-            type: 'input',
-            message: 'Enter the ID of the Employee you wish to Update.'
+            name: 'updatesthis',
+            type: 'list',
+            message: 'Select the Employee you wish to Update.',
+            choices: showEmployees
         },
         {
-            name: 'roletype',
-            type: 'input',
-            message: 'Enter the ID of the Role you wish to Update.'
+            name: 'roley',
+            type: 'list',
+            message: 'What the new Role ID you wish to Update.',
+            choices: showRoles
         },
         {
-            name: 'management',
-            type: 'input',
-            message: 'Enter the ID of the Manager you wish to Update.'
+            name: 'manage',
+            type: 'list',
+            message: 'Who is their new Manager?.',
+            choices: speak2ManagerPlz
+
         }
     ]).then(response =>{
-        con.query(`UPDATE employeetypes SET role_id=${response.roletype}, manager_id=${response.management} WHERE id=${response.update}`, (err, res) => {
+        con.query(`UPDATE employeetypes SET role_id=${response.roley}, manager_id=${response.manage} WHERE id=${response.updatesthis}`, (err, res) => {
             if(err) throw err;
             console.log('Updated Employee Information.')
             yourChoices();
